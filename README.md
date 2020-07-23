95d971c1f1026c6a58bfa9239527251239abdc54
Richard Leek <richard@richardleek.com>
2020-07-23 10:05:06 -0400

    initial commit! Wow that was a lot of boilerplate just to get an html page

---
+++ README.md
@@ -0,0 +1,2 @@
+# Pong
+This is a web based port of the classic game [Pong](https://en.wikipedia.org/wiki/Pong).
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/pong.js',
+    output: {
+        filename: 'pong.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist')
+    }
+};
